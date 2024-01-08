4bf6e492fa08c715dd0917818ba3ff43d307d210
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-01-08 15:48:12 -0600

    se crean metodos para el consumo de Apis

--- Caninar/caninar/lib/API/APi.dart
+++ Caninar/caninar/lib/API/APi.dart
@@ -1,13 +1,28 @@
+import 'dart:convert';
+
 import 'package:caninar/models/carrusel/model.dart';
 import 'package:caninar/models/categorias/model.dart';
+import 'package:caninar/models/certificados/model.dart';
 import 'package:caninar/models/distritos/model.dart';
 import 'package:caninar/models/marcas/model.dart';
 import 'package:caninar/models/productos/model.dart';
+import 'package:caninar/models/user/model.dart';
+import 'package:caninar/widgets/finalizar_compra.dart';
+import 'package:caninar/widgets/login.dart';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class API {
   static Dio dio = Dio();
+  static UserLoginModel? userLoginModelFinal;
+
+  currentUser() {
+    UserLoginModel? userLoginModel = userLoginModelFinal;
+    return userLoginModel;
+  }
+
   Future<void> launchWhatsApp(String phoneNumber, {String message = ""}) async {
     try {
       String whatsappUrl = getWhatsAppUrl(phoneNumber, message);
@@ -73,6 +88,7 @@ class API {
     Response response = await dio.get(link);
 
     List<MarcasModel> marcas = [];
+    List<CertificadosModel> certificates = [];
 
     //print(response.data);
 
@@ -80,6 +96,13 @@ class API {
       List<dynamic> marcaList = response.data['data'];
 
       for (Map<String, dynamic> marca in marcaList) {
+        if (marca['certificates'] != null) {
+          List<dynamic> certificatesList = marca['certificates'];
+          for (Map<String, dynamic> certificate in certificatesList) {
+            certificates.add(CertificadosModel.fromJson(certificate));
+          }
+        }
+
         marcas.add(MarcasModel.fromJson(marca));
       }
     }
@@ -94,12 +117,85 @@ class API {
 
     List<ProductoModel> productos = [];
 
-    print(response.data);
-
     for (Map<String, dynamic> producto in response.data) {
       productos.add(ProductoModel.fromJson(producto));
     }
 
     return productos;
+  }
+
+  passRecovery(String email, BuildContext context) async {
+    Map<String, dynamic> dataSend = {
+      "email": email,
+    };
+
+    String jsonBody = jsonEncode(dataSend);
+
+    await dio
+        .post(
+      'https://gc1hfo9hl0.execute-api.us-east-1.amazonaws.com/dev/auth/password_recovery',
+      data: jsonBody,
+    )
+        .then((value) async {
+      if (value.statusCode == 200) {
+        Fluttertoast.showToast(
+          msg: 'La solicitud de recuperación ha sido creada con éxito',
+          backgroundColor: Colors.green,
+        );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => Login()),
+        );
+      }
+    }).catchError((e) {
+      Fluttertoast.showToast(
+          msg: 'Ha ocurrido un error',
+          backgroundColor: Colors.red,
+          textColor: Colors.black);
+    });
+  }
+
+  Future<UserLoginModel?> login(
+      String email, String pass, BuildContext context) async {
+    UserLoginModel? userLoginModel;
+    Map<String, dynamic> login = {
+      "email": email,
+      "password": pass,
+    };
+
+    String jsonBody = jsonEncode(login);
+
+    await dio
+        .post(
+      'https://gc1hfo9hl0.execute-api.us-east-1.amazonaws.com/dev/auth/login',
+      data: jsonBody,
+    )
+        .then((value) async {
+      if (value.statusCode == 200) {
+        dynamic jsonData = json.decode(value.toString());
+
+        UserLoginModel userLoginModelTemp =
+            UserLoginModel.fromJson(jsonData['user']);
+
+        userLoginModel = userLoginModelTemp;
+
+        userLoginModelFinal = userLoginModel;
+
+        Fluttertoast.showToast(
+          msg: 'Bienvenido ${userLoginModel?.firstName}',
+          backgroundColor: Colors.green,
+        );
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => FinalizarCompra()),
+        );
+      }
+    }).catchError((e) {
+      Fluttertoast.showToast(
+          msg: 'Correo o contraseña incorrecto',
+          backgroundColor: Colors.red,
+          textColor: Colors.black);
+    });
+    return userLoginModel;
   }
 }
