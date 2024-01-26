cf3be1a85333c7d03bc7ef297349e20c6e78fe12
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-01-26 12:14:10 -0500

    se genera la clase para el provider del carrito

---
+++ Caninar/caninar/lib/providers/cart_provider.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CartProvider extends ChangeNotifier {
+  final List<Map<String, dynamic>> _cartItems = [];
+
+  List<Map<String, dynamic>> get cartItems => _cartItems;
+
+  addToCart(Map<String, dynamic> item) {
+    _cartItems.add(item);
+    notifyListeners();
+  }
+
+  removeFromCart(int index) {
+    if (index >= 0 && index < _cartItems.length) {
+      _cartItems.removeAt(index);
+      notifyListeners();
+    }
+  }
+}
