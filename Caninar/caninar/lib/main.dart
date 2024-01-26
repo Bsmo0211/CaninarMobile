ef08b04dd8a81e17502351ab0534ebb8c19c3ee8
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-01-26 12:11:44 -0500

    se agrega provider

--- Caninar/caninar/lib/main.dart
+++ Caninar/caninar/lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:caninar/constants/principals_colors.dart';
 import 'package:caninar/constants/routes.dart';
 import 'package:caninar/pages/page_categoria_seleccionada.dart';
+import 'package:caninar/providers/cart_provider.dart';
 import 'package:caninar/widgets/about_us.dart';
 
 import 'package:caninar/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
+import 'package:provider/provider.dart';
 import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:location/location.dart';
@@ -22,35 +24,43 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: EasyLoading.init(),
-      localizationsDelegates: const [
-        SfGlobalLocalizations.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate,
-        GlobalCupertinoLocalizations.delegate,
-      ],
-      supportedLocales: const [
-        Locale('en'), // English
-        Locale('es'),
-      ],
-      title: 'Caninar',
-      theme: ThemeData().copyWith(
-        cardTheme: const CardTheme(
-          surfaceTintColor: Colors.white,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (context) => (CartProvider()),
         ),
-        useMaterial3: false,
-        colorScheme: ThemeData().colorScheme.copyWith(
-              primary: PrincipalColors.blue,
-            ),
-        brightness: Brightness.light,
+      ],
+      child: MaterialApp(
+        builder: EasyLoading.init(),
+        localizationsDelegates: const [
+          SfGlobalLocalizations.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate,
+          GlobalCupertinoLocalizations.delegate,
+        ],
+        supportedLocales: const [
+          Locale('en'), // English
+          Locale('es'),
+        ],
+        title: 'Caninar',
+        theme: ThemeData().copyWith(
+          cardTheme: const CardTheme(
+            surfaceTintColor: Colors.white,
+          ),
+          useMaterial3: false,
+          colorScheme: ThemeData().colorScheme.copyWith(
+                primary: PrincipalColors.blue,
+              ),
+          brightness: Brightness.light,
+        ),
+        debugShowCheckedModeBanner: false,
+        locale: const Locale('es'),
+        home: const Home(),
+        routes: {
+          Routes.home.name: (_) => const Home(),
+        },
       ),
-      debugShowCheckedModeBanner: false,
-      locale: const Locale('es'),
-      home: const Home(),
-      routes: {
-        Routes.home.name: (_) => const Home(),
-      },
     );
+    ;
   }
 }
