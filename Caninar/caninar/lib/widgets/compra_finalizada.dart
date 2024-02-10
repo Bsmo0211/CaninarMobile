8300755ad142361d427528e94d494e1193ef0f66
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-02-09 19:06:35 -0500

    se crea interfaz provicional compra finalizada

---
+++ Caninar/caninar/lib/widgets/compra_finalizada.dart
@@ -0,0 +1,55 @@
+import 'package:caninar/pages/home.dart';
+import 'package:caninar/providers/cart_provider.dart';
+import 'package:caninar/widgets/boton_custom.dart';
+import 'package:caninar/widgets/custom_appBar.dart';
+import 'package:caninar/widgets/custom_drawer.dart';
+import 'package:caninar/widgets/login.dart';
+import 'package:caninar/widgets/redireccion_atras.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class CompraFinalizada extends StatefulWidget {
+  const CompraFinalizada({super.key});
+
+  @override
+  State<CompraFinalizada> createState() => _CompraFinalizadaState();
+}
+
+class _CompraFinalizadaState extends State<CompraFinalizada> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const CustomAppBar(),
+      drawer: CustomDrawer(),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+            child: Image.asset(
+              'assets/images/Recurso 7.png',
+              width: 200,
+            ),
+          ),
+          const Center(
+            child: Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: Text(
+                'Su compra fue realizada con exito!',
+                style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          BotonCustom(
+              funcion: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Home()),
+                );
+              },
+              texto: 'Volver al home')
+        ],
+      ),
+    );
+  }
+}
