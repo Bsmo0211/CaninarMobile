035339c4bb5318ef5ae53aa31570642d09aef32c
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-03 08:36:49 -0500

    se arreglan jsons

--- Caninar/caninar/lib/widgets/interfaz_marker.dart
+++ Caninar/caninar/lib/widgets/interfaz_marker.dart
@@ -1,8 +1,11 @@
 import 'package:caninar/constants/principals_colors.dart';
+import 'package:caninar/models/coverage/model.dart';
 import 'package:caninar/models/marcas/model.dart';
 import 'package:caninar/models/productos/model.dart';
 import 'package:caninar/models/user/model.dart';
 import 'package:caninar/providers/cart_provider.dart';
+import 'package:caninar/providers/direccion_provider.dart';
+import 'package:caninar/providers/producto_provider.dart';
 import 'package:caninar/shared_Preferences/shared.dart';
 import 'package:caninar/widgets/boton_custom.dart';
 import 'package:caninar/widgets/carrito.dart';
@@ -17,11 +20,13 @@ class InterfazMarker extends StatefulWidget {
   ProductoModel producto;
   MarcasModel marca;
   String idCategoria;
+  String idDistrito;
   InterfazMarker({
     super.key,
     required this.producto,
     required this.marca,
     required this.idCategoria,
+    required this.idDistrito,
   });
 
   @override
@@ -34,6 +39,7 @@ class _InterfazMarkerState extends State<InterfazMarker> {
   UserLoginModel? user;
   String? selectedDistritoName;
   String? selectedInside;
+  String? deliveryCost;
 
   getCurrentUser() async {
     UserLoginModel? userTemp = await Shared().currentUser();
@@ -46,22 +52,81 @@ class _InterfazMarkerState extends State<InterfazMarker> {
   agregarCarrito() {
     CartProvider cartProvider =
         Provider.of<CartProvider>(context, listen: false);
+    ProductoProvider ordenProvider =
+        Provider.of<ProductoProvider>(context, listen: false);
+    DireccionProvider direccionProvider =
+        Provider.of<DireccionProvider>(context, listen: false);
 
-    Map<String, dynamic> nuevoItem = {
-      "address": {
-        // enviar m
-        "name": '$selectedAdress',
-        "inside": '$selectedInside',
-        "name_district": '$selectedDistritoName', //enviar distrito
-        "id_district": "1",
-        "default": "true"
-      },
+    Map<String, dynamic> nuevoSupplier = {
+      "coverge": widget.marca.coverage,
+      "create_at": "",
+      "delivery_time": widget.marca.deliveryTime,
+      "bussines_name": widget.marca.bussinesName,
+      "contact": widget.marca.contact,
+      "status": 1,
+      "slug": widget.marca.slug,
+      "address": widget.marca.addresses,
+      "id_user": user?.id,
+      "email": [''],
+      "name": widget.marca.name,
+      "updated_at": 1622602830,
+      "image": widget.marca.image,
+      "categories": widget.marca.categories,
+      "min_order_amount": widget.marca.minOrderAmount,
+      "telephone": widget.marca.telephone,
+      "id": widget.marca.id,
+      "min_amount_free_delivery": widget.marca.minAmountFreeDelivery,
+      "ruc": widget.marca.ruc,
+      "items": [
+        {
+          "discount_type": 1,
+          "status": 1,
+          "slug": widget.producto.slug,
+          "stock": widget.producto.stock,
+          "discount_percent": "${widget.producto.discountPercent}",
+          "name": widget.producto.name,
+          "updated_at": 1622602830,
+          "price_offer": "${widget.producto.priceOffer}",
+          "image": widget.producto.image,
+          "categories": widget.producto.categories,
+          "id_category": widget.idCategoria,
+          "units": widget.producto.units,
+          "description": widget.producto.description,
+          "id": widget.producto.id,
+          "price": widget.producto.price,
+          "id_supplier": widget.marca.id,
+          "supplier": {
+            "coverage": widget.marca.coverage,
+            "delivery_time": widget.marca.deliveryTime,
+            "bussines_name": widget.marca.bussinesName,
+            "contact": widget.marca.contact,
+            "status": 1,
+            "slug": widget.marca.slug,
+            "address": widget.marca.addresses,
+            "id_user": user?.id,
+            "email": [''],
+            "name": widget.marca.name,
+            "updated_at": 1622602830,
+            "image": widget.marca.image,
+            "categories": widget.marca.categories,
+            "min_order_amount": widget.marca.minOrderAmount,
+            "telephone": widget.marca.telephone,
+            "id": widget.marca.id,
+            "min_amount_free_delivery": widget.marca.minAmountFreeDelivery,
+            "ruc": widget.marca.ruc
+          },
+          "quantity": quantity
+        }
+      ],
+    };
+
+    Map<String, dynamic> nuevaOrden = {
       "id": "${widget.marca.id}", // id
       "name": "${widget.marca.name}",
       "email": "${widget.marca.bussinesName}", //no esta
       "contact": "${widget.marca.contact}",
       "telephone": "${widget.marca.telephone}",
-      "delivery_cost": "${widget.marca.deliveryTime}", //no esta
+      "delivery_cost": "$deliveryCost", //no esta
       "delivery_time": "${widget.marca.deliveryTime}",
       "total": "95.45", //no esta
       "pet_name": {
@@ -78,7 +143,7 @@ class _InterfazMarkerState extends State<InterfazMarker> {
             "end": '',
           }
         },
-        "pet_id": '' //no deberia ir
+        "pet_id": ''
       },
       "items": [
         {
@@ -90,7 +155,19 @@ class _InterfazMarkerState extends State<InterfazMarker> {
         }
       ]
     };
-    cartProvider.addToCart(nuevoItem);
+
+    Map<String, dynamic> createAdress = {
+      // enviar m
+      "name": '$selectedAdress',
+      "inside": '$selectedInside',
+      "name_district": '$selectedDistritoName', //enviar distrito
+      "id_district": "1",
+      "default": "true"
+    };
+
+    direccionProvider.addDireccion(createAdress);
+    cartProvider.addToCart(nuevoSupplier);
+    ordenProvider.addOrden(nuevaOrden);
   }
 
   double calculateTotalValue(int quantity) {
@@ -98,14 +175,29 @@ class _InterfazMarkerState extends State<InterfazMarker> {
     return quantity * unitPrice;
   }
 
+  void deliveryCostMap() {
+    if (widget.marca.coverage != null) {
+      for (CoverageModel? coverage in widget.marca.coverage!) {
+        if (coverage?.idDistrict == widget.idDistrito) {
+          setState(() {
+            deliveryCost = coverage?.deliveryCost;
+          });
+          break;
+        }
+      }
+    }
+  }
+
   @override
   void initState() {
+    deliveryCostMap();
     getCurrentUser();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    print(deliveryCost);
     return Scaffold(
       appBar: const CustomAppBar(),
       drawer: CustomDrawer(),
