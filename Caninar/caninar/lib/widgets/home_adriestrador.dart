c44219e619e45829c448c9145bee38fff29abe0c
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-08 14:38:14 -0500

    se crea el home para adiestrador

---
+++ Caninar/caninar/lib/widgets/home_adriestrador.dart
@@ -0,0 +1,17 @@
+import 'package:caninar/widgets/custom_drawer.dart';
+import 'package:caninar/widgets/mis_citas.dart';
+import 'package:flutter/material.dart';
+
+class HomeAdiestrador extends StatefulWidget {
+  const HomeAdiestrador({super.key});
+
+  @override
+  State<HomeAdiestrador> createState() => _HomeAdiestradorState();
+}
+
+class _HomeAdiestradorState extends State<HomeAdiestrador> {
+  @override
+  Widget build(BuildContext context) {
+    return MisCitas();
+  }
+}
