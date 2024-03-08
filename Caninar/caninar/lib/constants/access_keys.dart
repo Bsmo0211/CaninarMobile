bd9780ee005c4c74cd700065f27375232d2c0a52
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-08 15:23:49 -0500

    acces keys

---
+++ Caninar/caninar/lib/constants/access_keys.dart
@@ -0,0 +1,10 @@
+class Keys {
+  static const String awsAccessKey = "AKIAUWQGQ2W254XEVIEF";
+  static const String awsSecretKey = "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";
+  static const String mpPublicKey =
+      "APP_USR-09cd9e88-019a-413f-a802-8c71f3a09748";
+  static const String mpAccesTokenKey =
+      "APP_USR-4313796460186081-111723-bd81e1d60b114d138cb512ecbf93a7cd-1467015876";
+  static const String mpClientId = "4313796460186081";
+  static const String mpClientSecret = "c3RXUFanjFRUYOjWMjWoDeJJTe4AeDQS";
+}
