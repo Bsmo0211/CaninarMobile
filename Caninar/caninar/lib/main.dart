cfd5e27af83a66376ea596b45585b7d80239335d
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-08 14:26:11 -0500

    se redirecciona depende el typo

--- Caninar/caninar/lib/main.dart
+++ Caninar/caninar/lib/main.dart
@@ -1,15 +1,18 @@
 import 'package:caninar/constants/principals_colors.dart';
 import 'package:caninar/constants/routes.dart';
+import 'package:caninar/models/user/model.dart';
 import 'package:caninar/pages/page_categoria_seleccionada.dart';
 import 'package:caninar/providers/calendario_provider.dart';
 import 'package:caninar/providers/cart_provider.dart';
 import 'package:caninar/providers/direccion_provider.dart';
 import 'package:caninar/providers/orden_provider.dart';
 import 'package:caninar/providers/producto_provider.dart';
+import 'package:caninar/shared_Preferences/shared.dart';
 import 'package:caninar/widgets/about_us.dart';
 
 import 'package:caninar/pages/home.dart';
 import 'package:caninar/widgets/finalizar_compra.dart';
+import 'package:caninar/widgets/home_adriestrador.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:go_router/go_router.dart';
@@ -18,24 +21,28 @@ import 'package:syncfusion_localizations/syncfusion_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:location/location.dart';
 
+Widget _defaultHome = const Home();
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Location().requestPermission();
+
+  //await Location().requestPermission();
+  UserLoginModel? user = await Shared().currentUser();
+
+  if (user != null) {
+    if (user.type == 1 || user.type == 1) {
+      _defaultHome = const Home();
+    } else {
+      _defaultHome = const HomeAdiestrador();
+    }
+  } else {
+    _defaultHome = const Home();
+  }
 
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   MyApp({super.key});
-
-  final router = GoRouter(
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (_, __) => const Home(),
-      ),
-    ],
-  );
 
   @override
   Widget build(BuildContext context) {
@@ -57,8 +64,7 @@ class MyApp extends StatelessWidget {
           create: (context) => (CalendarioProvider()),
         ),
       ],
-      child: MaterialApp.router(
-        routerConfig: router,
+      child: MaterialApp(
         builder: EasyLoading.init(),
         localizationsDelegates: const [
           SfGlobalLocalizations.delegate,
@@ -83,6 +89,7 @@ class MyApp extends StatelessWidget {
         ),
         debugShowCheckedModeBanner: false,
         locale: const Locale('es'),
+        home: _defaultHome,
       ),
     );
   }
