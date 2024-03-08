3f2718d4136f5298d661b390410b0701d331f1c0
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-08 14:37:10 -0500

    se arregla el appbar condicionado por tipos

--- Caninar/caninar/lib/widgets/custom_appBar.dart
+++ Caninar/caninar/lib/widgets/custom_appBar.dart
@@ -1,18 +1,43 @@
 import 'package:caninar/API/APi.dart';
 import 'package:caninar/constants/principals_colors.dart';
+import 'package:caninar/models/user/model.dart';
 import 'package:caninar/providers/cart_provider.dart';
 import 'package:caninar/providers/producto_provider.dart';
+import 'package:caninar/shared_Preferences/shared.dart';
 import 'package:caninar/widgets/carrito.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
-  const CustomAppBar({Key? key}) : super(key: key);
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  State<CustomAppBar> createState() => _CustomAppBarState();
+
+  @override
+  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  UserLoginModel? user;
+
+  getCurrentUser() async {
+    UserLoginModel? userTemp = await Shared().currentUser();
+
+    setState(() {
+      user = userTemp;
+    });
+  }
+
+  @override
+  void initState() {
+    getCurrentUser();
+    super.initState();
+  }
 
   @override
   Widget build(BuildContext context) {
     ProductoProvider productoProvider = Provider.of<ProductoProvider>(context);
-
     return AppBar(
       iconTheme: const IconThemeData(
         color: Colors.white,
@@ -24,40 +49,43 @@ class CustomAppBar extends StatelessWidget implements PreferredSizeWidget {
         width: 90,
       ),
       actions: [
-        GestureDetector(
-          child: Image.asset(
-            'assets/images/wpp.png',
-            width: 30,
-          ),
-          onTap: () {
-            API().launchWhatsApp('51919285667');
-          },
-        ),
         Padding(
-          padding: const EdgeInsets.only(
-            right: 8,
+          padding: user?.type == 2 || user == null
+              ? const EdgeInsets.only(right: 15)
+              : const EdgeInsets.all(0),
+          child: GestureDetector(
+            child: Image.asset(
+              'assets/images/wpp.png',
+              width: 30,
+            ),
+            onTap: () {
+              API().launchWhatsApp('51919285667');
+            },
           ),
-          child: Badge.count(
-            count: productoProvider.productoList.length,
-            backgroundColor: PrincipalColors.orange,
-            child: IconButton(
-              icon: const Icon(
-                Icons.shopping_cart_outlined,
-                size: 30,
+        ),
+        if (user?.type == 1 || user?.type == 3)
+          Padding(
+            padding: const EdgeInsets.only(
+              right: 8,
+            ),
+            child: Badge.count(
+              count: productoProvider.productoList.length,
+              backgroundColor: PrincipalColors.orange,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.shopping_cart_outlined,
+                  size: 30,
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CarritoCompras()),
+                  );
+                },
               ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => CarritoCompras()),
-                );
-              },
             ),
           ),
-        ),
       ],
     );
   }
-
-  @override
-  Size get preferredSize => const Size.fromHeight(kToolbarHeight);
 }
