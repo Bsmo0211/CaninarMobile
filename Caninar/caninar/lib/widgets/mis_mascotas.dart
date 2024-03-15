4194eb2747b5a317690f9629648c83dc90633ce1
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-15 16:30:34 -0500

    se hacen ajustes requeridos

--- Caninar/caninar/lib/widgets/mis_mascotas.dart
+++ Caninar/caninar/lib/widgets/mis_mascotas.dart
@@ -2,19 +2,19 @@ import 'package:caninar/API/APi.dart';
 import 'package:caninar/constants/principals_colors.dart';
 import 'package:caninar/models/mascotas/model.dart';
 import 'package:caninar/models/user/model.dart';
+import 'package:caninar/shared_Preferences/shared.dart';
 import 'package:caninar/widgets/boton_custom.dart';
 import 'package:caninar/widgets/cards_items_home.dart';
 import 'package:caninar/widgets/custom_appBar.dart';
 import 'package:caninar/widgets/custom_drawer.dart';
+import 'package:caninar/widgets/login.dart';
 import 'package:caninar/widgets/page_registro_mascotas.dart';
 import 'package:caninar/widgets/redireccion_atras.dart';
 import 'package:flutter/material.dart';
 
 class MisMascotas extends StatefulWidget {
-  UserLoginModel user;
   MisMascotas({
     super.key,
-    required this.user,
   });
 
   @override
@@ -23,114 +23,141 @@ class MisMascotas extends StatefulWidget {
 
 class _MisMascotasState extends State<MisMascotas> {
   List<MascotasModel> mascotas = [];
+  UserLoginModel? user;
 
   void refreshMascotas() async {
     await getMascotas();
   }
 
-  getMascotas() async {
-    List<MascotasModel> mascotasTemp =
-        await API().getMascotasByUser(widget.user.id!);
+  getCurrentUser() async {
+    UserLoginModel? userTemp = await Shared().currentUser();
 
     setState(() {
-      mascotas = mascotasTemp;
+      user = userTemp;
     });
+
+    await getMascotas();
+  }
+
+  getMascotas() async {
+    if (user != null) {
+      List<MascotasModel> mascotasTemp =
+          await API().getMascotasByUser(user!.id!);
+
+      setState(() {
+        mascotas = mascotasTemp;
+      });
+    }
   }
 
   @override
   void initState() {
-    getMascotas();
+    getCurrentUser();
+
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: const CustomAppBar(),
-      drawer: CustomDrawer(),
-      body: Column(
-        children: [
-          RedireccionAtras(nombre: 'Mis Mascotas'),
-          mascotas.isEmpty
-              ? Center(
-                  child: SizedBox(
-                  width: 300,
-                  child: ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor:
-                          MaterialStatePropertyAll<Color>(PrincipalColors.blue),
-                    ),
-                    child: const Text(
-                      'Crear Nueva Mascota',
-                      style: TextStyle(color: Colors.white),
-                    ),
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => PageRegistroMascotas(
-                            registro: true,
-                            refresh: refreshMascotas,
-                          ),
-                        ),
-                      );
-                    },
-                  ),
-                ))
-              : Expanded(
-                  child: ListView(
+    return user != null
+        ? Scaffold(
+            appBar: const CustomAppBar(),
+            drawer: CustomDrawer(),
+            body: RefreshIndicator(
+                child: Column(
                   children: [
-                    Column(
-                      children: mascotas.map((mascota) {
-                        return CardItemHome(
-                          terminadoCitas: false,
-                          imageCard: mascota.image,
-                          titulo: mascota.name!,
-                          redireccion: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => PageRegistroMascotas(
-                                  mascota: mascota,
-                                  refresh: refreshMascotas,
-                                  registro: false,
-                                ),
+                    RedireccionAtras(nombre: 'Mis Mascotas'),
+                    mascotas.isEmpty
+                        ? Center(
+                            child: SizedBox(
+                            width: 300,
+                            child: ElevatedButton(
+                              style: ButtonStyle(
+                                backgroundColor:
+                                    MaterialStatePropertyAll<Color>(
+                                        PrincipalColors.blue),
                               ),
-                            );
-                          },
-                          colorTexto: Colors.black,
-                        );
-                      }).toList(),
-                    ),
-                    Center(
-                        child: SizedBox(
-                      width: 300,
-                      child: ElevatedButton(
-                        style: ButtonStyle(
-                          backgroundColor: MaterialStatePropertyAll<Color>(
-                              PrincipalColors.blue),
-                        ),
-                        child: const Text(
-                          'Crear Nueva Mascota',
-                          style: TextStyle(color: Colors.white),
-                        ),
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => PageRegistroMascotas(
-                                registro: true,
-                                refresh: refreshMascotas,
+                              child: const Text(
+                                'Crear Nueva Mascota',
+                                style: TextStyle(color: Colors.white),
                               ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => PageRegistroMascotas(
+                                      registro: true,
+                                      refresh: refreshMascotas,
+                                    ),
+                                  ),
+                                );
+                              },
                             ),
-                          );
-                        },
-                      ),
-                    ))
+                          ))
+                        : Expanded(
+                            child: ListView(
+                            children: [
+                              Column(
+                                children: mascotas.map((mascota) {
+                                  return CardItemHome(
+                                    terminadoCitas: false,
+                                    imageCard: mascota.image,
+                                    titulo: mascota.name!,
+                                    redireccion: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              PageRegistroMascotas(
+                                            mascota: mascota,
+                                            refresh: refreshMascotas,
+                                            registro: false,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                    colorTexto: Colors.black,
+                                  );
+                                }).toList(),
+                              ),
+                              Center(
+                                  child: Padding(
+                                padding: EdgeInsets.only(bottom: 25),
+                                child: SizedBox(
+                                  width: 300,
+                                  child: ElevatedButton(
+                                    style: ButtonStyle(
+                                      backgroundColor:
+                                          MaterialStatePropertyAll<Color>(
+                                              PrincipalColors.blue),
+                                    ),
+                                    child: const Text(
+                                      'Crear Nueva Mascota',
+                                      style: TextStyle(color: Colors.white),
+                                    ),
+                                    onPressed: () {
+                                      Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) =>
+                                              PageRegistroMascotas(
+                                            registro: true,
+                                            refresh: refreshMascotas,
+                                          ),
+                                        ),
+                                      );
+                                    },
+                                  ),
+                                ),
+                              ))
+                            ],
+                          ))
                   ],
-                ))
-        ],
-      ),
-    );
+                ),
+                onRefresh: () async {
+                  getMascotas();
+                }),
+          )
+        : Login();
   }
 }
