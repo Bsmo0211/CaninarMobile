49ce36e62b06a6dbb5d312112521f26f8fffccca
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-15 16:31:13 -0500

    se crea nuevo componente para alojar el home

---
+++ Caninar/caninar/lib/widgets/item_home.dart
@@ -0,0 +1,162 @@
+import 'dart:async';
+
+import 'package:caninar/API/APi.dart';
+import 'package:caninar/models/carrusel/model.dart';
+import 'package:caninar/models/categorias/model.dart';
+import 'package:caninar/models/user/model.dart';
+import 'package:caninar/pages/page_categoria_seleccionada.dart';
+import 'package:caninar/shared_Preferences/shared.dart';
+import 'package:caninar/widgets/cards_items_home.dart';
+import 'package:caninar/widgets/custom_appBar.dart';
+import 'package:caninar/widgets/custom_drawer.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_easyloading/flutter_easyloading.dart';
+
+class ItemHome extends StatefulWidget {
+  const ItemHome({super.key});
+
+  @override
+  State<ItemHome> createState() => _ItemHomeState();
+}
+
+class _ItemHomeState extends State<ItemHome> {
+  bool isApiCallProcess = false;
+  List<CategoriasModel> categorias = [];
+  List<CarruselModel> imagenes = [];
+  int currentIndex = 0;
+  late Timer _timer;
+  UserLoginModel? user;
+  List<Widget> paginasNavegacion = [];
+  // Declara el temporizador
+
+  getCurrentUser() async {
+    UserLoginModel? userTemp = await Shared().currentUser();
+
+    setState(() {
+      user = userTemp;
+    });
+  }
+
+  getData() async {
+    setState(() {
+      isApiCallProcess = true;
+    });
+    List<CategoriasModel> result = await API().getCategorias();
+    List<CarruselModel> imagenesTemp = await API().getImageCarrusel();
+    imagenesTemp.sort((a, b) => a.order!.compareTo(b.order!));
+    setState(() {
+      categorias = result;
+      imagenes = imagenesTemp;
+    });
+    setState(() {
+      isApiCallProcess = false;
+    });
+  }
+
+  @override
+  void initState() {
+    getCurrentUser();
+    super.initState();
+    getData();
+    _timer = Timer.periodic(Duration(seconds: 5), (timer) {
+      if (mounted) {
+        setState(() {
+          if (currentIndex < imagenes.length - 1) {
+            currentIndex++;
+          } else {
+            currentIndex = 0;
+          }
+        });
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _timer?.cancel();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isApiCallProcess) {
+      EasyLoading.show(status: 'Cargando');
+    } else {
+      EasyLoading.dismiss();
+    }
+    return PopScope(
+      canPop: false,
+      child: Scaffold(
+        appBar: const CustomAppBar(),
+        drawer: CustomDrawer(),
+        body: ListView(
+          children: [
+            const Padding(
+              padding: EdgeInsets.only(
+                top: 15,
+                bottom: 15,
+              ),
+              child: Center(
+                child: Text(
+                  'Bienvenido',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 20,
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 200,
+              child: AnimatedSwitcher(
+                duration: const Duration(milliseconds: 500),
+                child: imagenes.isNotEmpty
+                    ? Image.network(
+                        imagenes[currentIndex].imageMobile!,
+                        key: ValueKey<int>(currentIndex),
+                      )
+                    : const CircularProgressIndicator(),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 15,
+              ),
+              child: Center(
+                child: Text(
+                  'Todo lo que necesitas en un solo app.',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey.shade800,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Column(
+              children: categorias.map((categoria) {
+                return CardItemHome(
+                  terminadoCitas: false,
+                  titulo: '${categoria.name}',
+                  imageCard: categoria.image,
+                  redireccion: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => PageCategoriaSeleccionada(
+                          slugCategoria: categoria.slug ?? '',
+                          name: categoria.name!,
+                          idCategoria: categoria.id!,
+                        ),
+                      ),
+                    );
+                  },
+                );
+              }).toList(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
