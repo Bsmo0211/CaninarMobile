1cbbad3b4c6b16104f3528324e9000764a54aa0e
Bsmo0211 <bsmartinezo@geosat.com.co>
2024-03-19 10:04:16 -0500

    se arregla inetrfaz para peticion post sobre creacion del aliado

--- Caninar/caninar/lib/widgets/aliados.dart
+++ Caninar/caninar/lib/widgets/aliados.dart
@@ -1,9 +1,16 @@
+import 'dart:convert';
+
+import 'package:caninar/API/APi.dart';
 import 'package:caninar/constants/principals_colors.dart';
+import 'package:caninar/models/distritos/model.dart';
+import 'package:caninar/pages/home.dart';
 import 'package:caninar/widgets/boton_custom.dart';
 import 'package:caninar/widgets/custom_appBar.dart';
 import 'package:caninar/widgets/custom_drawer.dart';
 import 'package:caninar/widgets/redireccion_atras.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class Alidados extends StatefulWidget {
   Alidados({Key? key}) : super(key: key);
@@ -14,14 +21,78 @@ class Alidados extends StatefulWidget {
 
 class _AlidadosState extends State<Alidados> {
   String dropdownValueTipoPersona = 'Persona Natural';
+  List<DropdownMenuItem<DistritosModel>> distritos = [];
   List<String> dropdownTipoPersona = ['Persona Natural', 'Persona Jurídica'];
-
+  DistritosModel? dropdownValueDistrito;
+  List<String?> selectedValues = [];
   String dropdownValueActividad = 'Paseador/Adiestrador Canino';
   List<String> dropdownActividad = [
     'Paseador/Adiestrador Canino',
     'Petshop',
     'Veterinaria'
   ];
+
+  submit() async {
+    Dio dio = Dio();
+
+    Map<String, dynamic> createAliado = {
+      "first_name": nombresCtrl.text,
+      "last_name": apellidosCtrl.text,
+      "email": correoCtrl.text,
+      "person_type": dropdownValueTipoPersona,
+      "phone": telefonoCtrl.text,
+      "activities": dropdownValueActividad,
+      "districts": selectedValues
+    };
+
+    await dio
+        .post(
+      'https://gc1hfo9hl0.execute-api.us-east-1.amazonaws.com/dev/auth/invitation',
+      data: createAliado,
+    )
+        .then((value) async {
+      if (value.statusCode == 200) {
+        Fluttertoast.showToast(
+          msg: 'Solicitud enviada con éxito',
+          backgroundColor: Colors.green,
+        );
+
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const Home(),
+          ),
+        );
+      }
+    }).catchError((e) {
+      print(e);
+      Fluttertoast.showToast(
+          msg: 'Ha ocurrido un error',
+          backgroundColor: Colors.red,
+          textColor: Colors.black);
+    });
+  }
+
+  getDistritos() async {
+    List<DropdownMenuItem<DistritosModel>> temp = [];
+    List<DistritosModel> distritosTemp = await API().getDistritos();
+
+    for (DistritosModel distrito in distritosTemp) {
+      temp.add(
+        DropdownMenuItem(
+          value: distrito,
+          child: Text(
+            '${distrito.name}',
+            style: const TextStyle(fontSize: 15),
+          ),
+        ),
+      );
+    }
+
+    setState(() {
+      distritos = temp;
+    });
+  }
 
   final formKey1 = GlobalKey<FormState>();
   TextEditingController nombresCtrl = TextEditingController();
@@ -167,7 +238,7 @@ class _AlidadosState extends State<Alidados> {
               ),
               const Text('Teléfono'),
               Padding(
-                padding: const EdgeInsets.only(top: 8, bottom: 8),
+                padding: const EdgeInsets.only(top: 8, bottom: 5),
                 child: TextFormField(
                   decoration: InputDecoration(
                     floatingLabelBehavior: FloatingLabelBehavior.never,
@@ -395,6 +466,12 @@ class _AlidadosState extends State<Alidados> {
   }
 
   @override
+  void initState() {
+    getDistritos();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const CustomAppBar(),
@@ -478,30 +555,84 @@ class _AlidadosState extends State<Alidados> {
                     ),
                   ),
                 ),
-                Padding(
-                  padding:
-                      const EdgeInsets.only(left: 20, right: 20, bottom: 15),
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(15),
-                      border: Border.all(
-                        color: PrincipalColors.blue,
-                        width: 2.0,
+                Center(
+                  child: SizedBox(
+                    width: 340,
+                    height: 60,
+                    child: DropdownButtonFormField(
+                      decoration: InputDecoration(
+                        enabledBorder: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.orange, width: 1),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide:
+                              const BorderSide(color: Colors.grey, width: 1),
+                          borderRadius: BorderRadius.circular(15),
+                        ),
+                        filled: true,
                       ),
-                    ),
-                    child: const Center(
-                      child: Text(
-                        'Contenido',
-                        style: TextStyle(fontSize: 16),
-                      ),
+                      dropdownColor: Colors.grey.shade200,
+                      value: dropdownValueDistrito,
+                      icon: const Icon(Icons.keyboard_arrow_down),
+                      items: distritos,
+                      onChanged: (DistritosModel? newValue) async {
+                        setState(() {
+                          dropdownValueDistrito = newValue;
+                          selectedValues.add(newValue?.name);
+                        });
+
+                        await Future.delayed(const Duration(seconds: 2));
+                      },
                     ),
                   ),
                 ),
+                if (selectedValues.isNotEmpty)
+                  const Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 15, bottom: 15),
+                      child: Text(
+                        'Distritos Seleccionados',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 17,
+                        ),
+                      ),
+                    ),
+                  ),
+                Center(
+                  child: Column(
+                    children: selectedValues.map((e) {
+                      return Padding(
+                        padding: const EdgeInsets.only(left: 10, right: 10),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Text(
+                                '$e',
+                                style: const TextStyle(fontSize: 16),
+                              ),
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.clear),
+                              onPressed: () {
+                                setState(() {
+                                  selectedValues.remove(e);
+                                });
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
                 BotonCustom(
-                  funcion: () {
-                    print('object');
+                  funcion: () async {
+                    if (validate()) {
+                      await submit();
+                    }
                   },
                   texto: 'Enviar Solicitud',
                 )
@@ -511,5 +642,18 @@ class _AlidadosState extends State<Alidados> {
         ],
       ),
     );
+  }
+
+  bool validate() {
+    final form = formKey1.currentState;
+
+    if (form!.validate()) {
+      if (selectedValues.isNotEmpty) {
+        form.save();
+      }
+      return true;
+    } else {
+      return false;
+    }
   }
 }
